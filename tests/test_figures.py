ff4f8d21e6b05995259a08ca5d009c40cfd2f0fe
Diego A. Diaz <diegodiaz@uchicago.edu>
2024-06-14 13:58:32 -0400

    saved figures with og gdps

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -7,7 +7,11 @@ output_dir = os.path.join(os.path.dirname(__file__), '..', 'output')
     "chile_scm.png",
     "nz_scm.png",
     "chile_placebo.png",
-    "nz_placebo.png"
+    "nz_placebo.png",
+    "chile_jacknife.png",
+    "nz_jacknife.png",
+    "chile_regions_gdp.png",
+    "nz_regions_gdp.png"
 ])
 def test_output_file_exists(file_name):
     file_path = os.path.join(output_dir, file_name)
